c3a187332615cccff1d6cc4868ee252a1eb2b711
John Ford <john@johnford.org>
2018-07-02 14:20:50 +0200

    command line tests

---
+++ test-cmdline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+rm -rf .cmdline-test
+mkdir .cmdline-test
+cd .cmdline-test
+
+cp ../test-files/* .
+
+for i in pkcs7 rsa2048 ; do
+  echo "-----BEGIN PKCS7-----" > real-$i
+  cat $i >> real-$i
+  echo "-----END PKCS7-----" >> real-$i
+done
+
+# Test the pkcs7 endpoint
+openssl smime -verify -in real-pkcs7 -inform PEM -content document -certfile pkcs7-pubkey -noverify > /dev/null
+if [ $? -eq 0 ] ; then echo PKCS7 file validates ;  else echo PKCS7 Failed ; exit 1 ; fi
+
+# Test the rsa2048 endpoint
+openssl smime -verify -in real-rsa2048 -inform PEM -content document -certfile rsa2048-pubkey -noverify > /dev/null
+if [ $? -eq 0 ] ; then echo RSA2048 file validates ; else echo RSA2048 Failed ; exit 1 ; fi
+
+
